f8f762a197488d154edbfab21d4c48ba64ad4e79
dioxia <luxelfioxia@gmail.com>
2026-06-19 23:17:34 +0000

    footer

---
+++ src/app/footer/Footer.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * src/app/footer/Footer.js
+ *
+ */
+
+import React from "react";
+
+import "../../assets/css/footer.css";
+
+export default Footer = () => {
+  return (
+    <footer className="footer-bg footer-container">
+      <a href="/" className="footer-link">
+        <img src="/icon.svg" alt="footer-icon" className="footer-icon" />
+      </a>
+    </footer>
+  );
+}
